10f34004f3ae8d59e4a107a625d55c58f2ae32af
Jie Lu <764664@gmail.com>
2017-09-01 14:27:18 +0900

    Fix download script

--- download-v2ray.js
+++ download-v2ray.js
@@ -1,17 +1,12 @@
 let version = "v2.36.3";
 
 const https = require('https');
-const fs = require('fs');
+const fs = require('fs-extra');
 const os = require('os');
 const request = require('request');
 const AdmZip = require('adm-zip');
 
-let url = null;
-let filename = null;
-let destination_dir = null;
-let executable_name = null;
-
-
+switchIntoWorkSpace();
 
 if (runningOnTravis()) {
   ["win32", "darwin"].forEach(platform => runTaskOnPlatform(platform));
@@ -20,31 +15,30 @@ else {
   runTaskOnPlatform(os.platform());
 }
 
-
-
 function runTaskOnPlatform(platform) {
-  setPlatform(platform);
-  switchIntoWorkSpace();
-  cleanUpOldFiles();
-  if (!alreadyExists()) {
-    download();
+  let params = setPlatform(platform);
+  cleanUpOldFiles(params);
+  if (!alreadyExists(params)) {
+    download(params);
   } else {
-    unzipAndMove();
+    unzipAndMove(params);
   }
 }
 
 function setPlatform(platform) {
   if (platform === "win32") {
-    url = `https://github.com/v2ray/v2ray-core/releases/download/${version}/v2ray-windows-64.zip`;
-    filename = `v2ray-${version}-windows-64.zip`;
-    destination_dir = `v2ray-${version}-windows-64`;
-    executable_name = "v2ray.exe";
+    let url = `https://github.com/v2ray/v2ray-core/releases/download/${version}/v2ray-windows-64.zip`;
+    let filename = `v2ray-${version}-windows-64.zip`;
+    let destination_dir = `v2ray-${version}-windows-64`;
+    let executable_name = "v2ray.exe";
+    return {url, filename, destination_dir, executable_name};
   }
   if (platform === "darwin") {
-    url = `https://github.com/v2ray/v2ray-core/releases/download/${version}/v2ray-macos.zip`;
-    filename = `v2ray-${version}-macos.zip`;
-    destination_dir = `v2ray-${version}-macos`;
-    executable_name = "v2ray";
+    let url = `https://github.com/v2ray/v2ray-core/releases/download/${version}/v2ray-macos.zip`;
+    let filename = `v2ray-${version}-macos.zip`;
+    let destination_dir = `v2ray-${version}-macos`;
+    let executable_name = "v2ray";
+    return {url, filename, destination_dir, executable_name};
   }
 }
 
@@ -52,18 +46,18 @@ function switchIntoWorkSpace() {
   // Create v2ray if not exists and enter the folder
   let dir = './assets/v2ray';
   if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+    fs.ensureDirSync(dir);
   }
   process.chdir(dir);
 }
 
-function cleanUpOldFiles() {
-  [executable_name, "config.json.default"].forEach(filename => removeIfExists(filename));
-  removeIfExists(destination_dir);
+function cleanUpOldFiles(params) {
+  [params.executable_name, "config.json.default"].forEach(filename => removeIfExists(filename));
+  removeIfExists(params.destination_dir);
 }
 
-function alreadyExists() {
-  return fs.existsSync(filename);
+function alreadyExists(params) {
+  return fs.existsSync(params.filename);
 }
 
 function removeIfExists(path) {
@@ -77,22 +71,20 @@ function removeIfExists(path) {
   }
 }
 
-function download() {
-  console.log("Downloading ", url);
-  if (url !== null) {
-    request(url).pipe(fs.createWriteStream(filename)).on('finish', () => {
-      unzipAndMove()
-    })
-  }
+function download(params) {
+  console.log("Downloading ", params.url);
+  request(url).pipe(fs.createWriteStream(params.filename)).on('finish', () => {
+    unzipAndMove(params)
+  })
 }
 
-function unzipAndMove() {
-  let zip = new AdmZip(filename);
-  zip.extractEntryTo(`${destination_dir}/${executable_name}`, "./");
-  zip.extractEntryTo(`${destination_dir}/config.json`, "./");
-  fs.renameSync(`${destination_dir}/${executable_name}`, `./${executable_name}`);
-  fs.renameSync(`${destination_dir}/config.json`, "./config.json.default");
-  fs.rmdirSync(destination_dir);
+function unzipAndMove(params) {
+  let zip = new AdmZip(params.filename);
+  zip.extractEntryTo(`${params.destination_dir}/${params.executable_name}`, "./");
+  zip.extractEntryTo(`${params.destination_dir}/config.json`, "./");
+  fs.renameSync(`${params.destination_dir}/${params.executable_name}`, `./${params.executable_name}`);
+  fs.renameSync(`${params.destination_dir}/config.json`, "./config.json.default");
+  fs.rmdirSync(params.destination_dir);
 }
 
 function runningOnTravis() {
