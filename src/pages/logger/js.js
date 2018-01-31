e8424ab8737bce94b843a22eb96e63cab5442b6e
Jie Lu <764664@gmail.com>
2018-01-31 22:44:19 +0900

    Automatically scrolls the new log when scrolled to bottom

--- src/pages/logger/js.js
+++ src/pages/logger/js.js
@@ -1,4 +1,14 @@
 require('electron').ipcRenderer.on('log', (event, message) => {
+  let scrollEnd = isScrollEnd();
   let div = document.getElementById('container')
   div.innerHTML = div.innerHTML + message
+  if (scrollEnd) {
+    let body = document.body;
+    body.scrollTop = body.scrollHeight;
+  }
 })
+
+function isScrollEnd() {
+  let body = document.body;
+  return body.scrollTop + body.clientHeight === body.scrollHeight;
+}
