7c780d58f151669eff807073a79873fdca597645
Jie Lu <764664@gmail.com>
2019-03-26 19:08:06 +0900

    Show active indicator in profile list

--- src/pages/config-editor/js.js
+++ src/pages/config-editor/js.js
@@ -1,134 +1,146 @@
-const loader = require('monaco-loader')
-const fs = require('fs')
-const {process, remote, ipcRenderer} = require('electron')
-const path = require('path')
-const Store = require('electron-store')
+const loader = require("monaco-loader");
+const fs = require("fs");
+const { process, remote, ipcRenderer } = require("electron");
+const path = require("path");
+const Store = require("electron-store");
 
-global.ROOT = path.join(__dirname, '..', '..', '..')
-vex.defaultOptions.className = 'vex-theme-default'
-const store = new Store()
-let editor = null
-let configPath = path.join(remote.app.getPath('userData'), "v2ray.json")
-let defaultConfigPath = path.join(global.ROOT, 'assets', 'v2ray', 'config.json.default')
+global.ROOT = path.join(__dirname, "..", "..", "..");
+vex.defaultOptions.className = "vex-theme-default";
+const store = new Store();
+const V2RAY_PROFILE = "v2ray-profiles";
+let editor = null;
+let configPath = path.join(remote.app.getPath("userData"), "v2ray.json");
+let defaultConfigPath = path.join(
+  global.ROOT,
+  "assets",
+  "v2ray",
+  "config.json.default"
+);
 
-loader().then((monaco) => {
-  editor = monaco.editor.create(document.getElementById('container'), {
-    language: 'json',
-    theme: 'vs',
+loader().then(monaco => {
+  editor = monaco.editor.create(document.getElementById("container"), {
+    language: "json",
+    theme: "vs",
     automaticLayout: true
-  })
-  
-  let number = loadProfiles()
+  });
+
+  let number = loadProfiles();
   if (number) {
-    showProfile(0)
+    showProfile(0);
   }
-})
+});
 
 function loadProfiles() {
-  let select = document.querySelector(".profiles")
-  select.innerHTML = ""
-  let profiles = store.get("v2ray-profiles")
+  let select = document.querySelector(".profiles");
+  select.innerHTML = "";
+  let profiles = store.get(V2RAY_PROFILE);
   if (!profiles) {
-    profiles = []
-    store.set("profiles", profiles)
+    profiles = [];
+    store.set("profiles", profiles);
   }
   profiles.forEach((profile, index) => {
-    let option = document.createElement("option")
-    option.text = profile.name
-    option.value = index
-    select.appendChild(option)
-  })
-  return profiles && profiles.length
+    let option = document.createElement("option");
+    option.text = profile.name + (profile.active ? " [Active]" : "");
+    option.value = index;
+    select.appendChild(option);
+  });
+  return profiles && profiles.length;
 }
 
 function showProfile(index) {
-  let profiles = store.get("v2ray-profiles")
-  loadDataToEditor(profiles[index].value)
+  let profiles = store.get(V2RAY_PROFILE);
+  loadDataToEditor(profiles[index].value);
 }
 
 function loadDataToEditor(data) {
-  editor.setModel(this.monaco.editor.createModel(data, 'json'));
+  editor.setModel(this.monaco.editor.createModel(data, "json"));
 }
 
 function saveProfile() {
-  let data = readProfileFromEditor()
-  let profiles = store.get("v2ray-profiles")
-  let index = document.querySelector(".profiles").value
-  profiles[index].value = data
-  store.set("v2ray-profiles", profiles)
+  let data = readProfileFromEditor();
+  let profiles = store.get(V2RAY_PROFILE);
+  let index = document.querySelector(".profiles").value;
+  profiles[index].value = data;
+  store.set(V2RAY_PROFILE, profiles);
 }
 
 function readProfileFromEditor() {
   const model = editor.getModel();
-  let data = ''
+  let data = "";
 
-  model._lines.forEach((line) => {
-    data += line.text + model._EOL
-  })
+  model._lines.forEach(line => {
+    data += line.text + model._EOL;
+  });
 
-  return data
+  return data;
 }
 
 function loadDefault() {
-  fs.readFile(defaultConfigPath, 'utf-8', (err, data) => {
-    loadDataToEditor(data)
-  })
+  fs.readFile(defaultConfigPath, "utf-8", (err, data) => {
+    loadDataToEditor(data);
+  });
 }
 
 document.querySelector(".create-profile").addEventListener("click", () => {
-  if (!store.get("v2ray-profiles")) {
-    store.set("v2ray-profiles", [])
+  if (!store.get(V2RAY_PROFILE)) {
+    store.set(V2RAY_PROFILE, []);
   }
   vex.dialog.prompt({
-    message: 'Enter new name',
-    callback: function (value) {
+    message: "Enter new name",
+    callback: function(value) {
       if (value) {
-        let profiles = store.get("v2ray-profiles")
-        let profile = {}
-        profile.name = value
-        profile.value = ""
-        profiles.push(profile)
-        store.set("v2ray-profiles", profiles)
-        loadProfiles(profiles.length - 1)
+        let profiles = store.get(V2RAY_PROFILE);
+        let profile = {};
+        profile.name = value;
+        profile.value = "";
+        profiles.push(profile);
+        store.set(V2RAY_PROFILE, profiles);
+        loadProfiles(profiles.length - 1);
       }
     }
-  })
-})
+  });
+});
 
 document.querySelector(".rename-profile").addEventListener("click", () => {
   vex.dialog.prompt({
-    message: 'Enter new name',
-    callback: function (value) {
+    message: "Enter new name",
+    callback: function(value) {
       if (value) {
-        let profiles = store.get("v2ray-profiles")
-        let index = document.querySelector(".profiles").value
-        profiles[index].name = value
-        store.set("v2ray-profiles", profiles)
-        loadProfiles(index)
+        let profiles = store.get(V2RAY_PROFILE);
+        let index = document.querySelector(".profiles").value;
+        profiles[index].name = value;
+        store.set(V2RAY_PROFILE, profiles);
+        loadProfiles(index);
       }
     }
-  })
-})
+  });
+});
 
 document.querySelector(".delete-profile").addEventListener("click", () => {
   vex.dialog.confirm({
-    message: 'Delete',
-    callback: function (value) {
-      let profiles = store.get("v2ray-profiles")
-      let index = document.querySelector(".profiles").value
-      profiles.splice(index, 1)
-      store.set("v2ray-profiles", profiles)
-      loadProfiles()
+    message: "Delete",
+    callback: function(value) {
+      let profiles = store.get(V2RAY_PROFILE);
+      let index = document.querySelector(".profiles").value;
+      profiles.splice(index, 1);
+      store.set(V2RAY_PROFILE, profiles);
+      loadProfiles();
     }
-  })
-})
+  });
+});
 
 document.querySelector(".use-profile").addEventListener("click", () => {
-  let data = readProfileFromEditor()
-  fs.writeFileSync(configPath, data, 'utf-8');
-  ipcRenderer.send("restart")
-})
+  let data = readProfileFromEditor();
+  fs.writeFileSync(configPath, data, "utf-8");
+  let profiles = store.get(V2RAY_PROFILE);
+  let index = document.querySelector(".profiles").value;
+  profiles = profiles.map((p, i) => Object.assign(p, { active: i == index }));
+  store.set(V2RAY_PROFILE, profiles);
+  loadProfiles();
+  document.querySelector(".profiles").value = index;
+  ipcRenderer.send("restart");
+});
 
 document.querySelector(".profiles").addEventListener("change", event => {
-  showProfile(event.target.value)
-})
+  showProfile(event.target.value);
+});
