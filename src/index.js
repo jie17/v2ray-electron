2bf651db12cf5efd358960dfc1d1c9aa8cf45d15
Jie Lu <764664@gmail.com>
2019-03-28 16:48:40 +0900

    Show dock icon when there is window(s) opening

--- src/index.js
+++ src/index.js
@@ -1,71 +1,77 @@
+const path = require("path");
 
-const path = require('path');
+global.ROOT = path.join(__dirname, "..");
 
-global.ROOT = path.join(__dirname, '..')
+const { app, Menu } = require("electron");
+const { initTray } = require("./tray");
+const { SystemProxy } = require("./proxy_conf_helper");
+const { Worker } = require("./worker");
+const { Logger } = require("./logger");
+const os = require("os");
+const log = require("electron-log");
+const autoUpdater = require("electron-updater").autoUpdater;
+const isDev = require("electron-is-dev");
 
-const {app, Menu} = require('electron')
-const {initTray} = require('./tray')
-const {SystemProxy} = require('./proxy_conf_helper')
-const {Worker} = require('./worker')
-const {Logger} = require('./logger')
-const os = require('os')
-const log = require('electron-log')
-const autoUpdater = require("electron-updater").autoUpdater
-const isDev = require('electron-is-dev');
+require("electron-debug")({ showDevTools: true });
+log.transports.file.level = "debug";
 
-require('electron-debug')({showDevTools: true})
-log.transports.file.level = 'debug';
+let tray = null;
+let systemProxy = os.platform() === "darwin" ? new SystemProxy() : null;
+let logger = new Logger();
+let worker = new Worker(logger);
 
-let tray = null
-let systemProxy = os.platform() === "darwin" ? new SystemProxy() : null
-let logger = new Logger()
-let worker = new Worker(logger)
-
-app.on('ready', () => {
-  log.info("App ready")
+app.on("ready", () => {
+  log.info("App ready");
 
   const template = [
     {
-      label: 'Edit',
+      label: "Edit",
       submenu: [
-        {role: 'undo'},
-        {role: 'redo'},
-        {type: 'separator'},
-        {role: 'cut'},
-        {role: 'copy'},
-        {role: 'paste'},
-        {role: 'pasteandmatchstyle'},
-        {role: 'delete'},
-        {role: 'selectall'}
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "pasteandmatchstyle" },
+        { role: "delete" },
+        { role: "selectall" }
       ]
     }
-  ]
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
   if (!isDev) {
     autoUpdater.checkForUpdates();
   }
-  if (os.platform() === 'darwin')
-    app.dock.hide()
-  initTray(worker, logger, systemProxy)
-})
+  if (os.platform() === "darwin") app.dock.hide();
+  initTray(worker, logger, systemProxy);
+});
 
-app.on('window-all-closed', () => {
-
-})
-
-app.on('quit', () => {
-  worker.stop()
-  if (systemProxy) {
-    systemProxy.turnOffSystemProxyIfEnabled()
+app.on("window-all-closed", () => {
+  if (os.platform() === "darwin") {
+    app.dock.hide();
   }
-  log.info("App quit")
-})
+});
 
-autoUpdater.on('update-not-available', (info) => {
-  setTimeout(() => autoUpdater.checkForUpdates(), 3600000);
-})
+app.on("browser-window-created", () => {
+  if (os.platform() === "darwin") {
+    app.dock.show();
+  }
+});
 
-autoUpdater.on('error', (err) => {
+app.on("quit", () => {
+  worker.stop();
+  if (systemProxy) {
+    systemProxy.turnOffSystemProxyIfEnabled();
+  }
+  log.info("App quit");
+});
+
+autoUpdater.on("update-not-available", info => {
   setTimeout(() => autoUpdater.checkForUpdates(), 3600000);
-})
+});
+
+autoUpdater.on("error", err => {
+  setTimeout(() => autoUpdater.checkForUpdates(), 3600000);
+});
