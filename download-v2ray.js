aa875f61de884a8eafaa16baae5821de8e036878
Jie Lu <764664@gmail.com>
2019-05-31 16:16:26 +0900

    Fix v2ray download path

--- download-v2ray.js
+++ download-v2ray.js
@@ -1,17 +1,16 @@
 const version = "v4.18.0";
 
-const https = require('https');
-const fs = require('fs-extra');
-const os = require('os');
-const request = require('request');
-const unzipper = require('unzipper');
+const https = require("https");
+const fs = require("fs-extra");
+const os = require("os");
+const request = require("request");
+const unzipper = require("unzipper");
 
 switchIntoWorkSpace();
 
 if (runningOnTravis()) {
   ["win32", "darwin"].forEach(platform => runTaskOnPlatform(platform));
-}
-else {
+} else {
   runTaskOnPlatform(os.platform());
 }
 
@@ -30,23 +29,23 @@ function setPlatform(platform) {
     let url = `https://github.com/v2ray/v2ray-core/releases/download/${version}/v2ray-windows-64.zip`;
     let filename = `v2ray-${version}-windows-64.zip`;
     let extract_dir = `v2ray-${version}-windows-64`;
-    let target_dir = 'v2ray-win';
+    let target_dir = "v2ray-win";
     let executable_name = "v2ray.exe";
-    return {url, filename, extract_dir, target_dir, executable_name};
+    return { url, filename, extract_dir, target_dir, executable_name };
   }
   if (platform === "darwin") {
     let url = `https://github.com/v2ray/v2ray-core/releases/download/${version}/v2ray-macos.zip`;
     let filename = `v2ray-${version}-macos.zip`;
     let extract_dir = `v2ray-${version}-macos`;
-    let target_dir = 'v2ray-macos';
+    let target_dir = "v2ray-macos";
     let executable_name = "v2ray";
-    return {url, filename, extract_dir, target_dir, executable_name};
+    return { url, filename, extract_dir, target_dir, executable_name };
   }
 }
 
 function switchIntoWorkSpace() {
   // Create v2ray if not exists and enter the folder
-  let dir = '../assets/v2ray';
+  let dir = "assets/v2ray";
   if (!fs.existsSync(dir)) {
     fs.ensureDirSync(dir);
   }
@@ -70,23 +69,28 @@ function removeIfExists(path) {
 
 function download(params) {
   console.log("Downloading ", params.url);
-  request(params.url).pipe(fs.createWriteStream(params.filename)).on('finish', () => {
-    unzipAndMove(params)
-  })
+  request(params.url)
+    .pipe(fs.createWriteStream(params.filename))
+    .on("finish", () => {
+      unzipAndMove(params);
+    });
 }
 
 function unzipAndMove(params) {
   fs.createReadStream(params.filename)
-  .pipe(unzipper.Extract({ path: `${process.cwd()}/${params.extract_dir}` }))
-  .on('finish', () => {
-    fs.renameSync(`./${params.extract_dir}/config.json`, `./${params.extract_dir}/config.json.default`);
-    // Avoid "EPERM: operation not permitted" on Windows
-    setTimeout(function() {
-      fs.renameSync(`./${params.extract_dir}`, `./${params.target_dir}`);
-    }, 1000);
-  });
+    .pipe(unzipper.Extract({ path: `${process.cwd()}/${params.extract_dir}` }))
+    .on("finish", () => {
+      fs.renameSync(
+        `./${params.extract_dir}/config.json`,
+        `./${params.extract_dir}/config.json.default`
+      );
+      // Avoid "EPERM: operation not permitted" on Windows
+      setTimeout(function() {
+        fs.renameSync(`./${params.extract_dir}`, `./${params.target_dir}`);
+      }, 1000);
+    });
 }
 
 function runningOnTravis() {
-  return 'TRAVIS' in process.env && 'CI' in process.env;
+  return "TRAVIS" in process.env && "CI" in process.env;
 }
